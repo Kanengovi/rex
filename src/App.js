bc5f842ecb23f173077cd2a1098ec9456e026094
Dong Vingo <dongvingo@gmail.com>
2025-08-15 19:16:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NameManager from './components/nameManager';
+
+function App() {
+  return (
+    <div className="App">
+      <NameManager/>
+    </div>
+  );
+}
+
+export default App;
